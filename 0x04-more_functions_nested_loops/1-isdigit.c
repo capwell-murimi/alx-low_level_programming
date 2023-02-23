0873efeb24079031af951a93166c9e11b52c390d
capwell-murimi <capwellmurimi@gmail.com>
2023-02-23 08:23:34 -0800

    is add

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit -checks 0 through 9
+ * @c : variable
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+		return (0);
+}
